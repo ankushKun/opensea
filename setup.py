397f864755d60e56e201e77c6556ba465b7783b1
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2021-10-02 12:33:16 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="opensea",
-    version="0.1.4",
+    version="0.1.5",
     description="wrapper library for opensea api",
     long_description=long_description,
     long_description_content_type="text/markdown",
